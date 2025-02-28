9bc16fbd238d66508abfe7b8f048fea5403d48a7
Thanhmai1 <test@w3schools.com>
2025-02-28 08:52:31 +0700

    create project

---
+++ examwcd/src/main/java/com/example/examwcd/ExamwcdApplication.java
@@ -0,0 +1,13 @@
+package com.example.examwcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamwcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamwcdApplication.class, args);
+	}
+
+}
